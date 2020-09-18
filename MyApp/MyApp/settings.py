cc00ac7355c2006c31e9062556c00cb87f83bead
Louis SANTUCCI <louis.santucci@epita.fr>
2020-09-18 14:44:19 +0200

    Adding login and logout options

--- MyApp/MyApp/settings.py
+++ MyApp/MyApp/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+LOGIN_REDIRECT_URL = 'my_app/'
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'MyApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('MyApp/MyApp/my_app/templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- MyApp/my_app/urls.py
+++ MyApp/my_app/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('detail/<int:user_id>/', views.detail, name='detail'),
     path('create_user/', views.create_user, name='create_user'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
